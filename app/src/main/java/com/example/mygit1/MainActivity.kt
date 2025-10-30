1540c86654a7975750e66f8670c40553c9236690
Ulia9595 <ulia.koroleva89@gmail.com>
2025-10-30 19:55:33 +0300

    UI: Создана сетка 2x2 с 4 квадрантами. Использованы Fractional Modifiers (fillMaxHeight/fillMaxWidth) для пропорционального разделения экрана.

--- app/src/main/java/com/example/mygit1/MainActivity.kt
+++ app/src/main/java/com/example/mygit1/MainActivity.kt
@@ -1,15 +1,13 @@
 package com.example.mygit1
 
-import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle.Companion.auto
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -20,14 +18,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.mygit1.ui.theme.MyGit1Theme
+
+val color1 = Color(0xFFEADDFF)
+val color2 = Color(0xFFD0BCFF)
+val color3 = Color(0xFFB69DF8)
+val color4 = Color(0xFFF6EDFF)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,10 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingImage(
-                        stringResource(R.string.tasks_completed),
-                        stringResource(R.string.nice)
-                    )
+                    QuadrantsScreen()
                 }
             }
         }
@@ -49,63 +47,83 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(header_1: String, header_2: String, modifier: Modifier = Modifier) {
+fun ComposableCard(
+    header: String,
+    body: String,
+    backgroundColor: Color,
+    modifier: Modifier = Modifier
+) {
     Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-    ) {
-        Text(
-            text = header_1,
-            fontSize = 24.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .padding(start = 24.dp, end = 8.dp)
-                .fillMaxWidth()
-        )
-        Text(
-            text = header_2,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
-    }
-}
-
-@Composable
-fun GreetingImage(header_1: String, header_2: String, modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier
             .fillMaxSize()
-            .background(Color.White),
+            .background(backgroundColor)
+            .padding(16.dp),
+
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_task_completed),
-            contentDescription = null,
-            contentScale = ContentScale.Fit,
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.CenterHorizontally)
+        Text(
+            text = header,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp),
+            textAlign = TextAlign.Center
         )
-        GreetingText(
-            header_1 = header_1,
-            header_2 = header_2,
-            modifier = Modifier
-                .fillMaxWidth()
+        Text(
+            text = body,
+            modifier = Modifier,
+            textAlign = TextAlign.Center
         )
     }
 }
 
-@Preview(showBackground = false)
 @Composable
-private fun TaskCompletedPreview() {
+fun QuadrantsScreen() {
+    Column(Modifier.fillMaxSize()) {
+
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.5f)
+        ) {
+            ComposableCard(
+                header = stringResource(R.string.header_1),
+                body = stringResource(R.string.text_1),
+                backgroundColor = color1,
+                modifier = Modifier.fillMaxWidth(0.5f)
+            )
+            ComposableCard(
+                header = stringResource(R.string.header_2),
+                body = stringResource(R.string.text_2),
+                backgroundColor = color2,
+                modifier = Modifier.fillMaxWidth(1f)
+            )
+        }
+
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(1f)
+        ) {
+            ComposableCard(
+                header = stringResource(R.string.header_3),
+                body = stringResource(R.string.text_3),
+                backgroundColor = color3,
+                modifier = Modifier.fillMaxWidth(0.5f)
+            )
+            ComposableCard(
+                header = stringResource(R.string.header_4),
+                body = stringResource(R.string.text_4),
+                backgroundColor = color4,
+                modifier = Modifier.fillMaxWidth(1f)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun QuadrantsPreview() {
     MyGit1Theme {
-        GreetingImage(
-            stringResource(R.string.tasks_completed),
-            stringResource(R.string.nice)
-        )
+        QuadrantsScreen()
     }
 }
